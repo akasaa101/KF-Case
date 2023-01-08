dbe8340b79541f8f1663f22872862f8c2f70421b
Süleyman Ali Akpınar <suleyman@linkadoo.me>
2023-01-08 04:24:26 +0300

    types: initialized

---
+++ src/type.d.ts
@@ -0,0 +1,27 @@
+type ErrorMessage = {
+  message: string
+}
+
+type Outage = {
+  id: string
+  begin: string
+  end: string
+}
+type SiteId = string
+
+type Device = {
+  id: string
+  name: string
+}
+
+type SiteInfo = {
+  id: string
+  name: string
+  devices: Device[]
+}
+type EnhancedOutages = {
+  id: string
+  name: string
+  begin: string
+  end: string
+}
--- test/router/site-info.test.ts
+++ test/router/site-info.test.ts
@@ -13,16 +13,16 @@ afterAll(() => {
 })
 
 describe('GET /site-info/{siteId}', () => {
-  it('should return 200 and It should return 200 and the correct site info for siteId "pear-tree" with a valid api key', async () => {
+  it('should return 200 and It should return 200 and the correct site info for siteId "norwich-pear-tree" with a valid api key', async () => {
     const response = await request(server)
-      .get('/site-info/pear-tree')
+      .get('/site-info/norwich-pear-tree')
       .set('x-api-key', 'EltgJ5G8m44IzwE6UN2Y4B4NjPW77Zk6FJK3lL23')
     expect(response.status).toBe(200)
     expect(response.body).toHaveProperty('id')
     expect(response.body).toHaveProperty('name')
     expect(response.body).toHaveProperty('devices')
     expect(response.body.devices).toBeInstanceOf(Array)
-    expect(response.body.id).toEqual('pear-tree')
+    expect(response.body.id).toEqual('norwich-pear-tree')
     response.body.devices.forEach(device => {
       expect(device).toHaveProperty('id')
       expect(device).toHaveProperty('name')
@@ -30,7 +30,7 @@ describe('GET /site-info/{siteId}', () => {
   })
   it('should return 403 and an error message if API key is not valid', async () => {
     const response = await request(server)
-      .get('/site-info/pear-tree')
+      .get('/site-info/norwich-pear-tree')
       .set('x-api-key', 'invalidApiKey')
       .expect(403)
     const body = response.body
